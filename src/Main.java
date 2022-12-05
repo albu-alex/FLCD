e875ff5d6eee6f56f2b9aa47bf3e33fc82233e94
Alex Albu <alexmhh1234@gmail.com>
2022-12-05 22:34:45 +0200

    Add Grammar + file structure change

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        FiniteAutomata finiteAutomata = new FiniteAutomata("/Users/alalbu/IdeaProjects/lab_4_LFTC/src/finiteAutomata.txt");
-        System.out.println(finiteAutomata.getStates());
-        System.out.println(finiteAutomata.getAlphabet());
-        System.out.println(finiteAutomata.getTransitions());
-        System.out.println(finiteAutomata.getFinalStates());
-        System.out.println(finiteAutomata.acceptsSequence("b"));
+        Grammar g = new Grammar("utils/G1.txt");
+        System.out.println(g.getNonterminals());
+        System.out.println(g.getTerminals());
+        System.out.println(g.getStartingSymbol());
+        System.out.println(g.getTransitions());
     }
 }
