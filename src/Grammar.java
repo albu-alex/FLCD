29df748f6b0f4fa1cb15335217bc3cb22a173ab5
Alex Albu <alexmhh1234@gmail.com>
2022-12-19 23:51:07 +0200

    Finish Grammar + g1&g2

--- src/Grammar.java
+++ src/Grammar.java
@@ -1,58 +1,155 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Grammar {
-    private final String ELEMENT_SEPARATOR = " ";
-    private final String TRANSITION_SEPARATOR = "|";
+    private Set<String> N = new HashSet<>();    // non-terminals
+    private Set<String> E = new HashSet<>();    // terminals (operators, separators, reserved words, identifiers, constants)
+    private HashMap<Set<String>, Set<List<String>>> P = new HashMap<>();    // productions (rules)
+    private String S = "";   // start symbol
 
-    // LL1
-    private List<String> nonterminals;
-    private List<String> terminals;
-    private List<Node> transitions;
-    private String startingSymbol;
+    public Grammar(String file) throws IOException {
+        readFile(file);
+    }
 
-    private int getFirstPositionOfNode(String state) {
-        for (Node node: this.transitions) {
-            if (node.info.equals(state)) {
-                return node.index;
+    private void readFile(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String nonTerminalsLine = reader.readLine();
+        N = new HashSet<>(Arrays.asList(nonTerminalsLine.strip().split(" ")));
+
+        String terminalsLine = reader.readLine();
+        E = new HashSet<>(Arrays.asList(terminalsLine.strip().split(" ")));
+
+        S = reader.readLine().strip();
+
+        // all the remaining lines are productions
+        String production = reader.readLine();
+        while (production != null) {
+            String[] tokens = production.split("::=");
+            String[] leftTokens = tokens[0].split(",");
+            String[] rightTokens = tokens[1].split("\\|");
+
+            Set<String> left = new HashSet<>();
+            for (String token : leftTokens) {
+                left.add(token.strip());
+            }
+            if (!P.containsKey(left)) {
+                P.put(left, new HashSet<>());
+            }
+
+            for (String token : rightTokens) {
+                ArrayList<String> productionRightElements = new ArrayList<>();
+                String[] rightTokenElements = token.strip().split(" ");
+                for (String r : rightTokenElements) {
+                    productionRightElements.add(r.strip());
+                }
+                P.get(left).add(productionRightElements);
+            }
+
+            production = reader.readLine();
+        }
+    }
+
+    public boolean checkContextFreeGrammar() {
+        boolean checkStartingSymbol = false;
+        for (Set<String> left : P.keySet()) {
+            if (left.contains(S)) {
+                checkStartingSymbol = true;
+                break;
             }
         }
-        return -1;
-    }
 
-    private void loadFromFile(String filePath) {
-        try (Scanner scanner = new Scanner(new File(filePath))) {
-            this.nonterminals = new ArrayList<>(List.of(scanner.nextLine().split(this.ELEMENT_SEPARATOR)));
-            this.terminals = new ArrayList<>(List.of(scanner.nextLine().split(this.ELEMENT_SEPARATOR)));
-            this.startingSymbol = scanner.nextLine();
-
-            this.transitions = new ArrayList<>();
-            this.transitions.add(new Node(0, this.startingSymbol, 0, 0)); // root
-            //TODO - load transitions
+        if (!checkStartingSymbol) {
+            return false;
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+        for (Set<String> left : P.keySet()) {
+            if (left.size() < 1) {
+                return false;
+            } else if (!N.contains(left.iterator().next())) {
+                return false;
+            }
+
+            Set<List<String>> right = P.get(left);
+            for(List<String> tokens : right) {
+                for (String token : tokens) {
+                    if (!(N.contains(token) || E.contains(token) || token.equals("epsilon"))) {
+                        return false;
+                    }
+                }
+            }
         }
+
+        return true;
     }
 
-    public Grammar(String filePath) {
-        this.loadFromFile(filePath);
+    public String printNonTerminals() {
+        StringBuilder stringBuilder = new StringBuilder("N = { ");
+        for (String nonTerminal : N) {
+            stringBuilder.append(nonTerminal).append(" ");
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 
-    public List<String> getNonterminals() {
-        return this.nonterminals;
+    public String printTerminals() {
+        StringBuilder stringBuilder = new StringBuilder("E = { ");
+        for (String terminal : E) {
+            stringBuilder.append(terminal).append(" ");
+        }
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 
-    public List<String> getTerminals() {
-        return this.terminals;
+    public String printProductions() {
+        StringBuilder stringBuilder = new StringBuilder("P = {\n");
+        P.forEach((left, right) -> {
+            stringBuilder.append("\t");
+            int comma = 0;
+            for (String token : left) {
+                stringBuilder.append(token);
+                comma += 1;
+                if (comma < left.size()) {
+                    stringBuilder.append(", ");
+                }
+            }
+            stringBuilder.append(" -> ");
+            comma = 0;
+            for (List<String> tokens : right) {
+                for (String token : tokens) {
+                    stringBuilder.append(token).append(" ");
+                }
+                comma += 1;
+                if (comma < right.size()) {
+                    stringBuilder.append("| ");
+                }
+            }
+            stringBuilder.append("\n");
+        });
+        stringBuilder.append("}");
+        return stringBuilder.toString();
     }
 
-    public List<Node> getTransitions() {
-        return this.transitions;
-    }
-
-    public String getStartingSymbol() {
-        return this.startingSymbol;
+    public String printProductionsForNonTerminal(String nonTerminal) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Set<String> left : P.keySet()) {
+            if (left.contains(nonTerminal)) {
+                stringBuilder.append(nonTerminal).append(" -> ");
+                Set<List<String>> right = P.get(left);
+                int comma = 0;
+                for (List<String> tokens : right) {
+                    for (String token : tokens) {
+                        stringBuilder.append(token).append(" ");
+                    }
+                    comma += 1;
+                    if (comma < right.size()) {
+                        stringBuilder.append("| ");
+                    }
+                }
+            }
+        }
+        return stringBuilder.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        Grammar g = new Grammar("utils/G1.txt");
-        System.out.println(g.getNonterminals());
-        System.out.println(g.getTerminals());
-        System.out.println(g.getStartingSymbol());
-        System.out.println(g.getTransitions());
+    public static void main(String[] args) throws IOException {
+        Grammar grammar = new Grammar("src/utils/g2.txt");
+
+        System.out.println();
+        if (grammar.checkContextFreeGrammar()) {
+            System.out.println("Context Free Grammar!");
+        } else {
+            System.out.println("Not a Context Free Grammar!");
+        }
+        System.out.println();
+        System.out.println(grammar.printNonTerminals());
+        System.out.println(grammar.printTerminals());
+        System.out.println(grammar.printProductions());
     }
 }
